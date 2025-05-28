904bb585389cfee4c84ebaacfaff71cb9a4b5de0
mitugui <cristian.mitugui04@gmail.com>
2025-05-28 10:22:32 -0300

    chore: adicionar autenticação Breeze

--- sigac/resources/js/app.js
+++ sigac/resources/js/app.js
@@ -2,3 +2,9 @@ import './bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './alerts'
 import './mascara_forms'
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- sigac/routes/web.php
+++ sigac/routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\DeclaracaoController;
 use App\Http\Controllers\DocumentoController;
 use App\Http\Controllers\EixoController;
 use App\Http\Controllers\NivelController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TurmaController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,20 +28,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/alunos', AlunoController::class);
-Route::resource('/categorias', CategoriaController::class);
-Route::resource('/comprovantes', ComprovanteController::class);
-Route::resource('/cursos', CursoController::class);
-Route::resource('/declaracoes', DeclaracaoController::class);
-Route::resource('/documentos', DocumentoController::class);
-Route::patch('/documentos/{id}/aprovar', [DocumentoController::class, 'aprovar'])->name('documentos.aprovar');
-Route::patch('/documentos/{id}/rejeitar', [DocumentoController::class, 'rejeitar'])->name('documentos.rejeitar');
-Route::resource('/eixos', EixoController::class);
-Route::resource('/niveis', NivelController::class);
-Route::resource('/turmas', TurmaController::class);
-
 Route::get('/aluno', function () {
     return view('aluno');
 });
 
-Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/alunos', AlunoController::class);
+    Route::resource('/categorias', CategoriaController::class);
+    Route::resource('/comprovantes', ComprovanteController::class);
+    Route::resource('/cursos', CursoController::class);
+    Route::resource('/declaracoes', DeclaracaoController::class);
+    Route::resource('/documentos', DocumentoController::class);
+    Route::patch('/documentos/{id}/aprovar', [DocumentoController::class, 'aprovar'])->name('documentos.aprovar');
+    Route::patch('/documentos/{id}/rejeitar', [DocumentoController::class, 'rejeitar'])->name('documentos.rejeitar');
+    Route::resource('/eixos', EixoController::class);
+    Route::resource('/niveis', NivelController::class);
+    Route::resource('/turmas', TurmaController::class);
+
+    Route::get('/admin', [AdminController::class, 'index']);
+});
+
+require __DIR__.'/auth.php';
